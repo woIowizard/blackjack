1f44e0e6b1541d7465eb47e34aa7de47fce9362e
thegreathowdini <73507668+thegreathowdini@users.noreply.github.com>
2023-03-05 22:49:29 +0800

    Update stratfind.py

--- stratfind.py
+++ stratfind.py
@@ -1,36 +1,69 @@
-import argparse,datetime
+edges = '''|-10|0.00994066872522887|0.00906049815895536|0.010903819496784251|0.0075887719921748625|0.005945398695516308|
+|-9|0.009248761787154614|0.00856781205314523|0.010562591168740313|0.007459361678027633|0.005707865374122695|
+|-8|0.007519810075717198|0.008257848362206758|0.009169675290686196|0.007196399788861663|0.005271171383003548|
+|-7|0.007216943548658794|0.008134476813796983|0.008202246690143983|0.007081912444638871|0.0049926208769952955|
+|-6|0.0061815451425469535|0.007069331516574062|0.007616956617500854|0.006140640662547221|0.004904273599678865|
+|-5|0.005421561641010196|0.005871703809407495|0.006814811930572097|0.006073564690198731|0.004713350958876891|
+|-4|0.0049465737514518|0.0057817128813427866|0.006717191623118072|0.005807983760576887|0.004531767357204793|
+|-3|0.0035710564478636155|0.004498892580040326|0.006662122285136016|0.005032568519194796|0.004371769331504996|
+|-2|0.003031661088540082|0.004416758420209416|0.004797083050487015|0.004553492203483922|0.004294915325194144|
+|-1|0.002172563220872002|0.0035747599889965377|0.004657037831580172|0.004227939220030406|0.004236452407689186|
+|0|0.0017503436572156898|0.0032839682276973184|0.0036253790728193393|0.0038995787440876165|0.00400819823727895|
+|1|0.0005362795421797175|0.0025679191477510897|0.003276305688272843|0.0036428361128320614|0.0037918027341684378|
+|2|-0.0008680833862761178|0.0019455443027193617|0.002589567869970672|0.003600379064029687|0.003708898109675021|
+|3|-0.0011047235118650934|0.0011329010242400518|0.0024678402127659193|0.003229489903832785|0.0036678847496487135|
+|4|-0.0015560282249800998|0.0006655745397941576|0.0017725405476669174|0.0028599562805691264|0.0036493602757631345|
+|5|-0.003027763909616168|0.00036846913084130064|0.0014747360764874424|0.0026432391646220212|0.003627666104594382|
+|6|-0.0033945730002604736|-0.00027988145165558343|0.0008062051460985028|0.0026154935073989015|0.0035636522161200147|
+|7|-0.004823597311068861|-0.0008700420107552072|0.0007757728067916418|0.0016354904158950463|0.0035416399765091804|
+|8|-0.005778641924954072|-0.00113920002581661|-0.0007971414315791877|0.0017142566308130077|0.003409873699951947|
+|9|-0.00659413918779642|-0.0018242902771010805|-0.0010902929803202456|0.0014400698055744429|0.00314851344032793|
+|10|-0.007343678850129651|-0.0020363829321124248|-0.0014974100153926297|0.000988551736913766|0.002993111193396823|
+|11|-0.007549077529171351|-0.003339783934296084|-0.00225965135183103|0.0009359721417203494|0.0029698237857178|
+|12|-0.008983276336604201|-0.0038168307418231144|-0.0027988747917354908|0.000926955420306853|0.002769250976507185|
+|13|-0.009368139824911553|-0.004484249231865061|-0.003138324442514168|0.0006466574384574554|0.002751618080345252|
+|14|-0.010811942849545512|-0.004820875681446687|-0.003968275015734912|0.0001962330023582169|0.0024224655014291096|
+|15|-0.010899512332873545|-0.005464019614031691|-0.0046238337371237665|4.56376668738817e-05|0.002160186894884765|'''
+
+transition_probs = '''|-10|2e-07|0.0|1.58e-05|0.0001776|0.0009538|
+|-9|3e-07|3.4e-06|8.18e-05|0.0008499|0.0024454|
+|-8|1.7e-06|5.76e-05|0.0007973|0.0032678|0.0063846|
+|-7|1.14e-05|0.000293|0.0040455|0.0092099|0.0139132|
+|-6|0.0001952|0.0039209|0.0131497|0.0206081|0.0261724|
+|-5|0.0007254|0.0182804|0.0311982|0.0384534|0.0431496|
+|-4|0.0167775|0.045782|0.0567652|0.0615574|0.0634234|
+|-3|0.079006|0.0903064|0.0888456|0.0868345|0.0842732|
+|-2|0.1206957|0.124291|0.1163087|0.108519|0.1020525|
+|-1|0.2262205|0.1588589|0.1356521|0.1222394|0.1126316|
+|0|0.1914771|0.158095|0.1381395|0.1245056|0.1145413|
+|1|0.1490651|0.1375766|0.1252197|0.1152134|0.1075097|
+|2|0.1049809|0.1063467|0.1023405|0.0976128|0.0930583|
+|3|0.0605969|0.0720696|0.0753905|0.0756748|0.0749966|
+|4|0.030715|0.0431561|0.0500386|0.0540491|0.0561677|
+|5|0.0129507|0.0229132|0.0303078|0.0356448|0.0390919|
+|6|0.0046089|0.0108602|0.0166425|0.0214998|0.0254701|
+|7|0.0014529|0.0045697|0.0084635|0.0121712|0.0156032|
+|8|0.000396|0.0017515|0.0039159|0.0063813|0.0089571|
+|9|9.15e-05|0.0006037|0.0016556|0.003105|0.0047748|
+|10|2.15e-05|0.0001852|0.0006663|0.0014213|0.0024133|
+|11|7e-06|5.44e-05|0.0002396|0.0006123|0.0011521|
+|12|1.1e-06|1.71e-05|8.01e-05|0.000248|0.0005048|
+|13|1e-06|4.9e-06|2.81e-05|9.17e-05|0.0002179|
+|14|4e-07|1.6e-06|7.7e-06|3.4e-05|9.23e-05|
+|15|1e-07|9e-07|4.2e-06|1.79e-05|4.92e-05|'''
+
+
+import argparse,datetime,itertools
 import numpy as np
 
-edges = np.array([-0.01302434801760524,-0.011908562279274669,-0.01080130089010126,-0.009782218561846107,-0.00894901433748832,-0.008396243206847816,-0.007258715015973558,-0.006775214055637279,-0.005628302794730593,-0.004765129528246249,-0.0038635863120416356,-0.00330061537657059,-0.001995837861451646,-0.0013194588595324594,-0.002513224716748848,0.0005293426651101526,0.0013550902515568198,0.0021298033756847663,0.003209325400909519, 0.0042026840768633065, 0.004810944675226022, 0.005500114718764763, 0.006733485250155465, 0.007515990183737213, 0.008526629353955165, 0.00931754065882899])
-three_pockets = np.array([1.388e-05, 7.984e-05, 0.000794, 0.00404058, 0.01309942, 0.03110434, 0.05686078, 0.08891252, 0.11648958, 0.13554564, 0.13813754, 0.12522472, 0.10216868, 0.07526086, 0.05014146, 0.03035748, 0.01674986, 0.00842468, 0.00389502, 0.00166914, 0.00066124, 0.00024186, 8.546e-05, 2.914e-05, 8.54e-06, 3.74e-06])
-four_pockets = np.array([0.0001777, 0.0008598, 0.0032596, 0.00923714, 0.02057002, 0.03844204, 0.06158872, 0.08681002, 0.10847384, 0.12223872, 0.12454376, 0.11529116, 0.09750388, 0.07566108, 0.05405622, 0.0355505, 0.02160006, 0.01218246, 0.00641168, 0.00312868, 0.00142076, 0.00060672, 0.00024112, 9.418e-05, 3.282e-05, 1.732e-05])
-five_pockets = np.array([0.0009696, 0.00246388, 0.00640488, 0.01392662, 0.02613614, 0.04312336, 0.06348248, 0.08431588, 0.10182704, 0.11265416, 0.11451148, 0.10741242, 0.09316238, 0.07493928, 0.05619134, 0.03920704, 0.02553556, 0.0155708, 0.0089034, 0.00479584, 0.0024185, 0.0011658, 0.00051724, 0.00021802, 9.042e-05, 5.644e-05])
-
 minimum,start,end,min_acceptable = 25,0,None,0
-
 p = argparse.ArgumentParser()
 p.add_argument('-m',help='minimum bet (default %s)'%minimum)
-p.add_argument('-s',help='starting value of x (default %s)'%start)
+p.add_argument('-s',help='starting value of x (default %s)'%(start+1))
 p.add_argument('-e',help='ending value of x (default %s)'%end)
 p.add_argument('-b',help='minimum acceptable edge (default %s)'%min_acceptable)
+p.add_argument('-F',action='store_true',help='full search')
 args = p.parse_args()
-
-try: assert len(edges) == 26
-except: 
-    print('edges vector in incorrect format (current length %s, expected 26)'%len(edges))
-    quit()
-try: assert len(three_pockets) == 26
-except: 
-    print('3 pockets transition vector in incorrect format (current length %s, expected 26)'%len(three_pockets))
-    quit()
-try: assert len(four_pockets) == 26
-except: 
-    print('4 pockets transition vector in incorrect format (current length %s, expected 26)'%len(four_pockets))
-    quit()
-try: assert len(five_pockets) == 26
-except: 
-    print('5 pockets transition vector in incorrect format (current length %s, expected 26)'%len(five_pockets))
-    quit()
 
 try:
     assert int(args.m) >= 0
@@ -53,54 +86,34 @@ try:
 except: 
     if args.b: print('[%s] -b option invalid. min acceptable edge defaulting to %s'%(datetime.datetime.now().replace(microsecond=0),min_acceptable))
 
-print('[%s] looking for strategy with edge >=%s from x=%s%s. min bet: $%s'%(datetime.datetime.now().replace(microsecond=0),min_acceptable,start,' to %s'%end if end else '',minimum))
-
-num_neg = len([c for c in edges if c < 0])
+print('[%s] looking for strategy with edge at least %s from x = %s%s. min bet: $%s'%(datetime.datetime.now().replace(microsecond=0),min_acceptable,start,' to %s'%end if end else '',minimum))
 
 def get_freq(pocket_strat):
-    transition_matrix = []
-    ref = [three_pockets,four_pockets,five_pockets]
-    for pocket_number in pocket_strat: transition_matrix.append(ref[pocket_number-3])
-    transition_matrix = np.array(transition_matrix).transpose()
-    transition_matrix -= np.eye(len(pocket_strat))
+    transition_matrix = np.array([transition_probs[i-1] for i in pocket_strat]).transpose() - np.eye(len(pocket_strat))
     transition_matrix[-1] = [1]*len(pocket_strat)
-    transition_matrix = np.linalg.inv(transition_matrix)
-    return(transition_matrix.transpose()[-1])
-    
-def get_EV(num3,num4,x,minimum,edges): 
-    pocket_strat = [3]*num3 + [4]*num4 + [5]*(26-num3-num4)
-    freqs = get_freq(pocket_strat)
-    ave_EV,ave_spend = 0,0
-    for i in range(26):
-        if i < num_neg:
-            ave_EV += freqs[i]*edges[i]*pocket_strat[i]*minimum
-            ave_spend += freqs[i]*pocket_strat[i]*minimum
-        else:
-            ave_EV += freqs[i]*edges[i]*pocket_strat[i]*25*x*(i-10)
-            ave_spend += freqs[i]*pocket_strat[i]*25*x*(i-10)
-    return ave_EV, ave_spend
+    return(np.linalg.inv(transition_matrix).transpose()[-1])
 
-best_EV,best_spend = -99,1
-x = max(minimum//25-1,start-1)
+best_edge,x = min_acceptable,max(minimum//25-1,start-1)
+edges,transition_probs,fixed_strat = [[(float(c) if c else 0) for c in l.split('|')[2:-1]] for l in edges.splitlines()],[np.array([l.split('|')[i+2] for l in transition_probs.splitlines()]).astype('float64') for i in range(5)],[]
+for l in edges:
+    a = [c for c in l if c < 0]
+    if a: fixed_strat.append(min(range(len(a)),key=lambda x:a[x]*(x+1))+1)
 
-while best_EV/best_spend < min_acceptable:
+while best_edge <= min_acceptable:
     x += 1
-    if end and x > end: 
-        print('[%s] failed to find any strategy with edge at least %s'%(datetime.datetime.now().replace(microsecond=0),min_acceptable))
-        quit()
-    print('[%s] Trying x = %s'%(datetime.datetime.now().replace(microsecond=0),x))
-    best_num3,best_num4 = -99,-99
-    for num3 in range(num_neg+1):
-        for num4 in range(num_neg+1-num3):
-            this_EV,this_spend = get_EV(num3,num4,x,minimum,edges)
-            if this_EV > best_EV: best_EV,best_num3,best_num4,best_spend = this_EV,num3,num4,this_spend
-else:
-    print('[%s] optimum strategy found with EV $%s/round and player edge %s%%'%(datetime.datetime.now().replace(microsecond=0),round(best_EV,4),round(best_EV/best_spend*100,4)))
-    pocket_strat = [3]*best_num3 + [4]*best_num4 + [5]*(26-best_num3-best_num4)
-    betting_strat = [minimum]*num_neg + [i*x*25 for i in range(-10+num_neg,16)]
-    freqs = get_freq(pocket_strat)
-    per_round_ev = [pocket_strat[i]*betting_strat[i]*freqs[i]*edges[i] for i in range(26)]
-    print('\nCOUNT\tFREQUENCY\t\t\tPOCKETS\t\tBET\t\tEV')
-    print('='*90)
-    for i in range(26): print('{0}\t{1:<20}\t\t{2}\t\t{3}\t\t{4}'.format(i-10,freqs[i],pocket_strat[i],betting_strat[i],per_round_ev[i]))
-   
+    if end and x > end: print('[%s] failed to find any strategy with edge at least %s'%(datetime.datetime.now().replace(microsecond=0),min_acceptable)); quit()
+    print('[%s] trying x = %s'%(datetime.datetime.now().replace(microsecond=0),x))
+    for l in itertools.product(range(1,6),repeat=(26-len(fixed_strat))//(1 if args.F else 2)):
+        if not args.F: l = [l[j//2] for j in range(len(l)*2)][1 if len(fixed_strat)%2 else 0:]
+        pocket_strat = list(l) + fixed_strat
+        freqs = get_freq(pocket_strat)
+        ave_spend = sum(freqs[i]*pocket_strat[i]*(minimum if (i < 26-len(fixed_strat)) else 25*x*(i-10)) for i in range(26))
+        ave_EV = sum(freqs[i]*pocket_strat[i]*(minimum if (i < 26-len(fixed_strat)) else 25*x*(i-10))*-edges[i][pocket_strat[i]-1] for i in range(26))
+        if ave_EV/ave_spend > best_edge: best_edge,best_strat = ave_EV/ave_spend,pocket_strat
+      
+print('[%s] optimum strategy found with player edge %s%%'%(datetime.datetime.now().replace(microsecond=0),round(best_edge*100,4)))
+betting_strat = [minimum]*(26-len(fixed_strat)) + [i*x*25 for i in range(16-len(fixed_strat),16)]
+freqs = get_freq(best_strat)
+per_round_ev = [best_strat[i]*betting_strat[i]*freqs[i]*-edges[i][best_strat[i]-1] for i in range(26)]
+print('\nCOUNT\tFREQUENCY\t\t\tPOCKETS\t\tBET\t\tEV\n'+'='*90)
+for i in range(26): print('{0}\t{1:<20}\t\t{2}\t\t{3}\t\t{4}'.format(i-10,freqs[i],best_strat[i],betting_strat[i],per_round_ev[i]))
